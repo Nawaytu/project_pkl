6623059ccc06f18c3e2b7c542e914349c27c4504
Nawaytu <alrivaninawaytu@gmail.com>
2022-12-14 15:18:05 +0700

    sidebar edit

---
+++ application/views/index.php
@@ -0,0 +1,3 @@
+<a class="nav-link" href="<?= base_url() ?>projects">
+                        GOES to Projects
+                    </a>
--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -4,9 +4,9 @@
             <div class="sb-sidenav-menu">
                 <div class="nav">
                     <div class="sb-sidenav-menu-heading">Core</div>
-                    <a class="nav-link" href=" ">
+                    <a class="nav-link" href="<?= base_url() ?>projects">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                        Dashboard
+                        Projects
                     </a>
                 </div>
             </div>
